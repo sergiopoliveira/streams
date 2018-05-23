0d72dde297735b6ac194b49471418868ac079345
sergiopoliveira <sergiopoliveira@outlook.com>
2018-05-23 11:57:25 +0100

    Flatmap

--- src/com/sergio/Main.java
+++ src/com/sergio/Main.java
@@ -3,6 +3,8 @@ package com.sergio;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -47,6 +49,63 @@ public class Main {
 				.peek(System.out::println)
 				.count());
 		
+		Employee john = new Employee("John Doe", 30);
+		Employee jane = new Employee("Jane Deer", 25);
+		Employee jack = new Employee("Jack Hill", 40);
+		Employee snow = new Employee("Snow White", 22);
+
+		Department hr = new Department("Human Resources");
+		hr.addEmployee(jane);
+		hr.addEmployee(jack);
+		hr.addEmployee(snow);
+		
+		Department accounting = new Department("Accounting");
+		accounting.addEmployee(john);
+		
+		List<Department> departments = new ArrayList<>();
+		departments.add(hr);
+		departments.add(accounting);
+		
+		//using flatMap. It is called flatMap because it is often used to flaten nested lists
+		departments.stream()
+			.flatMap(department -> department.getEmployees(). stream())
+					.forEach(System.out::println);
+		
+//		List<String> sortedGNumbers = someBingoNumbers
+//				.stream()
+//				.map(String::toUpperCase)
+//				.filter(s -> s.startsWith("G"))
+//				.sorted()
+//				.collect(Collectors.toList()); // we now have a list we can continue to work with
+				
+		List<String> sortedGNumbers = someBingoNumbers
+				.stream()
+				.map(String::toUpperCase)
+				.filter(s -> s.startsWith("G"))
+				.sorted()
+				.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+		
+				for(String s : sortedGNumbers) {
+					System.out.println(s);
+				}
+		
+			Map<Integer, List<Employee>> groupedByAge = departments.stream()
+					.flatMap(department -> department.getEmployees().stream())
+					.collect(Collectors.groupingBy(employee -> employee.getAge()));
+		 System.out.println(groupedByAge);
+		 
+		 departments.stream()
+		 	.flatMap(department -> department.getEmployees().stream())
+		 	.reduce((e1,e2) -> e1.getAge() < e2.getAge() ? e1 : e2) // Bifunction , this is a terminal operation
+		 	.ifPresent(System.out::println); // print the result
+		 
+		 //stream operations are lazy-instantiated
+		 Stream.of("ABC", "AC", "BAA", "CCCC", "XY", "ST")
+		 .filter(s -> {
+			 System.out.println(s);
+			 return s.length() == 3;
+		 })
+		 .count();
 	}
 
 }
